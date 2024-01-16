1b3fdf7661c6c3be3fef81ddf39472efe87bc1b2
Shaid Azmin <excel.azmin@gmail.com>
2024-01-16 15:59:52 +0600

    complete full package

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # excel_frappe
 
 A package for ERPNext custom JS web app
+
+
+20502030204536901
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const requestPromise = require('request-promise');
 const querystring = require('querystring');
-const Promise = require('bluebird');
-
 class ERPNext {
   constructor(options) {
     this.username = options.username;
@@ -28,24 +26,11 @@ class ERPNext {
     return res;
   }
 
-  async get(resource, name) {
-    await this.login();
-    const url = `${this.baseUrl}/api/resource/${resource}/${name}`;
-    const res = await requestPromise.get({
-      url,
-      jar: this.cookieJar
-    });
-    console.log(url);
-    // const parsedData = JSON.parse(res);
-    return await JSON.parse(res);
-  }
-
-  async post(doctype, data) {
+  async createDocs(doctype, data) {
     await this.login();
     const formData = querystring.stringify({ data: JSON.stringify(data) });
     const contentLength = formData.length;
     const url = `${this.baseUrl}/api/resource/${doctype}`;
-    console.log(formData);
     const res = await requestPromise.post({
       url,
       jar: this.cookieJar,
@@ -55,71 +40,24 @@ class ERPNext {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
     });
-
-    // console.log(res);
-
     const parsedData = JSON.parse(res);
     return parsedData.data;
   }
 
-  async put(doctype, name, data) {
-    await this.login();
-    const formData = querystring.stringify({ data: JSON.stringify(data) });
-    const contentLength = formData.length;
-    const url = `${this.baseUrl}/api/resource/${doctype}/${name}`;
-    console.log(url);
-    const res = await requestPromise.put({
-      url,
-      jar: this.cookieJar,
-      body: formData,
-      headers: {
-        'Content-Length': contentLength,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-    const parsedData = JSON.parse(res);
-    console.log(url);
-    return parsedData.data;
-  }
-
-
-  async delete(doctype, name) {
-    await this.login();
-    const url = `${this.baseUrl}/api/resource/${doctype}/${name}`;
-    console.log(url);
-    const res = await requestPromise.delete({
-      url,
-      jar: this.cookieJar,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-    const parsedData = JSON.parse(res);
-    console.log(url);
-    return parsedData;
-  }
-
-  async getCustomersName(pageSize, page) {
+  async getDocs(doctype, fields, filters, order_by_name, pageSize, page) {
     var _this = this;
     return _this.login().then(function (res) {
       var start = (page - 1) * pageSize;
-      const url = 'http://excel_erpnext.localhost:8000/api/resource/Customer';
-      const fields = '["name", "customer_name", "territory"]';
-      const filters = '[["customer_name", "=", "SOURCE AND SERVICE - MOTIJHEEL"]]';
       var params = {
-        url: _this.baseUrl + `/api/resource/Customer?fields=${fields}&filters=${filters}`,
-
+        url: _this.baseUrl + `/api/resource/${doctype}?fields=${fields}&filters=${filters}`,
         jar: _this.cookieJar,
         qs: {
           limit_start: start,
           limit_page_length: pageSize,
-          order_by: "name asc",
+          order_by: order_by_name,
         },
         
       };
-
-      console.log(params.url)
-
       return requestPromise.get(params).then(function (customers) {
         customers = JSON.parse(customers);
         var totalCustomers = customers.data.length;
@@ -136,60 +74,58 @@ class ERPNext {
     });
   }
 
-  async getCustomerByName(doctype, name) {
-    const data = await this.get(doctype, name);
-    return data; 
+  async getDocByName(resource, name) {
+    await this.login();
+    const url = `${this.baseUrl}/api/resource/${resource}/${name}`;
+    const res = await requestPromise.get({
+      url,
+      jar: this.cookieJar
+    });
+    return await JSON.parse(res);
   }
 
-  async createCustomer(data) {
-    return this.post('Customer', data);
-  }
-
-  async updateCustomerByName(name, data) {
-    return this.put('Customer', name, data);
-  }
-
-  async getTotalCount() {
+  async getTotalDocs(doctype, fields, filters ) {
     var _this = this;
     return _this.login().then(function (res) {
-      const doctype = 'Sales Invoice';
-      const fields = '["count"(`tabSales Invoice`.`name`) AS total_count ]';
-      // const filters = '[["customer_name", "=", "SOURCE AND SERVICE - MOTIJHEEL"]]';
-      const filters = '';
       var params = {
         url: _this.baseUrl + `/api/method/frappe.client.get_count?doctype=${doctype}&filters=${filters}&fields=${fields}`,
         jar: _this.cookieJar,
       };
-  
-      console.log(params.url)
-  
       return requestPromise.get(params).then(function (data) {
-        const result = JSON.parse(data);
-        console.log(result);
-        return result;
+        return JSON.parse(data);
       });
     });
   }
 
+  async updateDocsByName(doctype, name, data) {
+    await this.login();
+    const formData = querystring.stringify({ data: JSON.stringify(data) });
+    const contentLength = formData.length;
+    const url = `${this.baseUrl}/api/resource/${doctype}/${name}`;
+    const res = await requestPromise.put({
+      url,
+      jar: this.cookieJar,
+      body: formData,
+      headers: {
+        'Content-Length': contentLength,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+    const parsedData = JSON.parse(res);
+    return parsedData.data;
+  }
 
-   // return "http://excel_erpnext.localhost:8000/api/method/frappe.client.get_count?doctype=Customer&fields=%5B%22count(%20%60tabCustomer%60.name%29%20AS%20total_count%22%5D"
-  // Add more methods for other resources and functionalities
-
-  // Example usage:
-  async main() {
-//     const pageSize = 10; // Number of customers per page
-//     const page = 1; // Current page number
-
-//   try {
-//     const result = await this.getCustomersName(pageSize, page);
-//     const totalCustomers = result.totalCustomers;
-//     const customers = result.customers;
-
-//     console.log('Total Customers:', totalCustomers);
-//     console.log('Customers:', customers);
-//   } catch (error) {
-//     console.error('An error occurred:', error);
-//   }
+  async deleteDocsByName(doctype, name) {
+    await this.login();
+    const url = `${this.baseUrl}/api/resource/${doctype}/${name}`;
+    const res = await requestPromise.delete({
+      url,
+      jar: this.cookieJar,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+    return JSON.parse(res);
   }
 }
 
